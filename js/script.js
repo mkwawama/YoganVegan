7f8ba25957e8a0668dad5de17a8d5c1c9392749f
Mahinya Mkwawa <mahinya.mkwawa@students.plymouth.ac.uk>
2021-04-16 12:32:41 +0100

    1st Stage Changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log ("Load")
+console.log("Loaded");
+
+//Use QuerySelector to select an object frm the DOM(Documnet Object Model)
+var btn = document.querySelector(".mobileNavBTN");
+var menu= document. querySelector(".menu");
+
+//Attach an event listener to the element, that listnes for a click
+btn.addEventListener("click",()=>{
+    //On Click,log message to our console
+    menu.classList.toggle("hide");
+});
+
+
+
+
+window.addEventListener('resize' , ()=>{
+    if(window.innerWidth> 420){
+        btn.classList.add('hide');
+        menu.classList.remove('hide');
+
+    } else if(window.innerWidth < 420){
+        btn.classList.remove('hide');
+        menu.classList.add('hide');
+
+
+    }
+})
